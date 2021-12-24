ae2b79e962eb18a7b7f4d73ab2cc25da9e226c17
Akila Welihinda <akilawelihinda@ucla.edu>
2021-12-24 10:04:50 -0800

    add all IR node types

--- skeleton/src/decaf/IrNodes.java
+++ skeleton/src/decaf/IrNodes.java
@@ -1,6 +1,8 @@
 package decaf;
 
 abstract class Ir {};
+
+// class
 class IrClassDecl extends Ir {
     private String name;
     void setName(String n) {
@@ -8,8 +10,36 @@ class IrClassDecl extends Ir {
     }
 };
 
+// expressions
 abstract class IrExpression extends Ir {};
+
 abstract class IrLiteral extends IrExpression {};
 class IrIntLiteral extends IrLiteral {};
+class IrBoolLiteral extends IrLiteral {};
 
-// TODO: add remaining classes
+abstract class IrCallExpr extends IrExpression {};
+class IrMethodCallExpr extends IrCallExpr {};
+class IrCalloutExpr extends IrCallExpr {};
+
+class IrBinopExpr extends IrExpression {}; 
+
+// statements
+abstract class IrStatement extends Ir {};
+class IrAssignStmt extends IrStatement {}; 
+class IrPlusAssignStmt extends IrStatement {};
+class IrBreakStmt extends IrStatement {};
+class IrContinueStmt extends IrStatement {};
+class IrIfStmt extends IrStatement {};
+class IrForStmt extends IrStatement {};
+class IrReturnStmt extends IrStatement {};
+class IrInvokeStmt extends IrStatement {};
+class IrBlock extends IrStatement {};
+
+// member
+abstract class IrMemberDecl extends Ir {};
+class IrMethodDecl extends IrMemberDecl {};
+class IrFieldDecl extends IrMemberDecl {};
+
+// vars and type
+class IrVarDecl extends Ir {};
+class IrType extends Ir {};
